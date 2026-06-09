d895b4b7228e82eaaec90b0ec6e87cb88a8c1cd5
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-06-09 23:34:39 +0700

    Initial Commit

---
+++ Clippy/ClippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippyApp.swift
+//  Clippy
+//
+//  Created by Bee Wijaya on 09/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ClippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
