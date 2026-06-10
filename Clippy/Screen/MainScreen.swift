b81a9dafc993ce02973fb206e33e6f47ae89b113
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-06-10 20:39:05 +0700

    feat: implementing clippy main ui + list
    goal is to listing all the clipboard existed, clippy design will showing the list of the clipboard
    
    it's just a list for us populated from the user clipboard and able to remove and copy

--- Clippy/Screen/MainScreen.swift
+++ Clippy/Screen/MainScreen.swift
@@ -7,25 +7,39 @@
 
 import SwiftUI
 
-
-struct ClipList: View {
-    var body: some View {
-        VStack {
-            Text("Hello world")
-        }
-    }
-}
-
 struct MainScreen: View {
+    private var clipped = [
+        "Hello World",
+        "@user",
+        "https://google.com",
+        "1234567890",
+        "+628123456789"
+    ]
+    
     var body: some View {
         VStack {
+            Text("Clippy")
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+            Text("Your clipboard, remembered")
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(.bottom, 12)
+            
             ScrollView {
-                ForEach(0..<5) { _ in
-                    ClipList()
+                ForEach(clipped.indices, id: \.self) { id in
+                    ClipItem(label: clipped[id]) {
+                        
+                    } onDelete: {
+                        
+                    }
+
                 }
             }
         }
         .padding()
+        .frame(width: 300, height: 500)
     }
 }
 
